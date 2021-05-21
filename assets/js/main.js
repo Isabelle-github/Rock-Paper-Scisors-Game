2cba6ca1f9d3ba361d631937a9f193d985eccf0c
Isabelle-github <ixavierayix@yahoo.fr>
2021-05-21 19:59:19 +0200

    computer move draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,13 @@ function handClick() {
     // open hand was cliked. display the countUp of corresponding round
     // add +1 on the count up each time the user clicked
     // hide all oder round radio buttons
+    document.getElementById("round").classList.add("hide");
 
     if (round5.checked) {
         round10.classList.add("hide");
         round15.classList.add("hide");
         round20.classList.add("hide");
+        hideFunction(label);
         countRound = countRound + 1;
         countUp.innerHTML = `${countRound} / ${round5.value}`;
     } else if (round10.checked) {
@@ -39,9 +41,38 @@ function handClick() {
     }
 }
 
-function rockClick() {
+function computerMove() {
+    switch (saveRandomNum % 3) {
+        case 0:
+
+            break;
+
+        case 1:
+            break;
+        default:
+            break;
+    }
+}
+function userMove() {
 
 }
-function scisorClick() {
+function restart() {
 
+}
+// Math.floor rundet ab
+// Math.random erstellt eine zufällige Zahl zwischen 0 - 1
+console.log(Math.floor(Math.random() * 10) + 1) // Zahl zwischen 1 - 10
+console.log(Math.floor(Math.random() * 11)) // Zahl zwischen 0 - 10
+
+
+let saveRandomNum = () => {
+    return Math.floor(Math.random() * 50) + 1
+}
+
+function hideFunction(tag) {
+    var x, i;
+    x = document.querySelectorAll(`${tag}`);
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
 }
