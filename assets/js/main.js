5fb6c8ff347e83e1625bf2bfd33265f2a8c9449a
Isabelle-github <ixavierayix@yahoo.fr>
2021-05-23 10:56:45 +0200

    computer random moves works

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,27 @@ const round5 = document.getElementById("five");
 const round10 = document.getElementById("ten");
 const round15 = document.getElementById("fifteen");
 const round20 = document.getElementById("twenty");
-const countUp = document.getElementById("countUp")
+const countUp = document.getElementById("countUp");
+const score = document.getElementById("score");
+const comments = document.getElementById("comments");
 let countRound = 0;
+let userScore = 0;
+let commputerScore = 0;
 
 function handClick() {
     // open hand was cliked. display the countUp of corresponding round
     // add +1 on the count up each time the user clicked
     // hide all oder round radio buttons
-    document.getElementById("round").classList.add("hide");
-
+    // document.getElementById("round").classList.add("hide");
+    score.innerHTML = `${userScore} : ${commputerScore}`;
+    comments.innerHTML = `User:Paper `;
+    userScore += 1;
+    computerMove();
     if (round5.checked) {
         round10.classList.add("hide");
         round15.classList.add("hide");
         round20.classList.add("hide");
-        hideFunction(label);
+        // hideFunction(label);
         countRound = countRound + 1;
         countUp.innerHTML = `${countRound} / ${round5.value}`;
     } else if (round10.checked) {
@@ -39,17 +46,27 @@ function handClick() {
     } else {
         countUp.innerHTML = "Choose how many rounds you want to play!"
     }
+
 }
 
 function computerMove() {
+    let saveRandomNum = Math.floor(Math.random() * 50) + 1;
+
+    console.log(saveRandomNum);
+    console.log(typeof saveRandomNum);
     switch (saveRandomNum % 3) {
         case 0:
-
+            comments.innerHTML += ` Computer:Paper`;
+            console.log(saveRandomNum + "case0")
             break;
 
         case 1:
+            comments.innerHTML += ` Computer:Rock`;
+            console.log(saveRandomNum + "case1")
             break;
-        default:
+        case 2:
+            comments.innerHTML += ` Computer:Scisors`;
+            console.log(saveRandomNum + "case2");
             break;
     }
 }
@@ -61,18 +78,16 @@ function restart() {
 }
 // Math.floor rundet ab
 // Math.random erstellt eine zufällige Zahl zwischen 0 - 1
-console.log(Math.floor(Math.random() * 10) + 1) // Zahl zwischen 1 - 10
-console.log(Math.floor(Math.random() * 11)) // Zahl zwischen 0 - 10
+// console.log(Math.floor(Math.random() * 10) + 1) // Zahl zwischen 1 - 10
+// console.log(Math.floor(Math.random() * 11)) // Zahl zwischen 0 - 10
 
 
-let saveRandomNum = () => {
-    return Math.floor(Math.random() * 50) + 1
-}
 
-function hideFunction(tag) {
-    var x, i;
-    x = document.querySelectorAll(`${tag}`);
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-}
+
+// function hideFunction(tag) {
+//     var x, i;
+//     x = document.querySelectorAll(`${tag}`);
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+// }
